5757232322ce9f4a94af3ce29f414520c5a80ea8
stormphoenix00 <andrewnew85@gmail.com>
2023-11-12 00:37:26 -0600

    Adds the message class

---
+++ message.py
@@ -0,0 +1,9 @@
+#The message class for building the NTFY message
+
+
+class message():
+	def __init__(self, message: str, title: str|None=None, priority: str|None=None):
+		self.message=message
+		self.dict={}
+		if title is not None: self.dict["title"]=title
+		if priority is not None: self.dict["priority"]=priority

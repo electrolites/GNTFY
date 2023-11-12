2d1c374817ca201adc188a831c8eb1e31582e72b
blindelectron <jump.leper@gmail.com>
2023-11-12 01:13:44 -0600

    makes message class capatle to follow pep8.

--- message.py
+++ message.py
@@ -1,7 +1,7 @@
 #The message class for building the NTFY message
 import json
 
-class message():
+class Message():
 	def __init__(self, message: str, title: str|None=None, priority: str|None=None):
 		self.message=message
 		self.dict={}

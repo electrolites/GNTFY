e56bfb03314288a805477d00bd025adee657ecf1
blindelectron <jump.leper@gmail.com>
2023-11-12 12:33:03 -0600

    Fixes server __init to be __init__

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from GNTFY.message import Message
 import httpx
 
-class Server():
-	def __init(self, topic: str, url: str="https://ntfy.sh"):
+class Server:
+	def __init__(self, topic: str, url: str="https://ntfy.sh"):
 		self.url=url
 		self.topic=topic
 
